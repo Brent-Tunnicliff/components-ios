e176d6fc47bc2f352de650db395ed2a560dcc0ec
Brent Tunnicliff <brenttunnicliff@gmail.com>
2023-11-05 21:48:05 +1100

    Created package.

---
+++ Package.swift
@@ -0,0 +1,41 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+// Copyright (c) 2023 Brent Tunnicliff
+
+import PackageDescription
+
+let package = Package(
+    name: "Components",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v17),
+        .macOS(.v14),
+        .tvOS(.v17),
+        .watchOS(.v10),
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Components",
+            targets: ["Components"]
+        ),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Components",
+            resources: [
+                .process("Resources"),
+            ]
+        ),
+        .testTarget(
+            name: "ComponentsTests",
+            dependencies: ["Components"]
+        ),
+    ]
+)
