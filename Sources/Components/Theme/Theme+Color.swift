81d771ad60e7f7f6e845064407318a1122dc6cc8
Brent Tunnicliff <brenttunnicliff@gmail.com>
2023-11-05 21:49:35 +1100

    Added the theme colorsets.
    This will be the theme that will be used for all my apps.

---
+++ Sources/Components/Theme/Theme+Color.swift
@@ -0,0 +1,54 @@
+// Copyright (c) 2023 Brent Tunnicliff
+
+import SwiftUI
+
+extension Theme {
+    enum Color: String, CaseIterable {
+        // MARK: - Primary
+        case primary
+        case onPrimary
+        case primaryContainer
+        case onPrimaryContainer
+        case inversePrimary
+
+        // MARK: - Secondary
+        case secondary
+        case onSecondary
+        case secondaryContainer
+        case onSecondaryContainer
+
+        // MARK: - Tertiary
+        case tertiary
+        case onTertiary
+        case tertiaryContainer
+        case onTertiaryContainer
+
+        // MARK: - Error
+        case error
+        case errorContainer
+        case onError
+        case onErrorContainer
+
+        // MARK: - Background
+        case background
+        case onBackground
+
+        // MARK: - Surface
+        case surface
+        case onSurface
+        case surfaceVariant
+        case onSurfaceVariant
+        case inverseOnSurface
+        case inverseSurface
+        case surfaceTint
+
+        // MARK: - Outline
+        case outline
+        case outlineVariant
+
+        // MARK: - Other
+        case shadow
+        case scrim
+        case seed
+    }
+}
---
+++ Tests/ComponentsTests/Theme/ThemeColorTests.swift
@@ -0,0 +1,21 @@
+// Copyright (c) 2023 Brent Tunnicliff
+
+@testable import Components
+import XCTest
+
+final class ThemeColorTests: XCTestCase {
+    func testColorExists() throws {
+        Theme.Color.allCases.forEach { color in
+            let result = getColor(named: color.rawValue)
+            XCTAssertNotNil(result, "Name mapped to nil: \(color.rawValue)")
+        }
+    }
+
+    private func getColor(named: String) -> Any? {
+        #if canImport(UIKit)
+            return UIColor(named: named, in: .module, compatibleWith: nil)
+        #elseif os(OSX)
+            return NSColor(named: named, bundle: .module)
+        #endif
+    }
+}
